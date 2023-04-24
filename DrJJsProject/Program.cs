d43e8b016421b835a6c52f80e85cb90971593320
juanp <juanp@DESKTOP-LAB62CP>
2023-04-24 16:01:21 -0500

    Add project files.

---
+++ DrJJsProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DrJJsProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Dr. JJ's");
+        }
+    }
+}
