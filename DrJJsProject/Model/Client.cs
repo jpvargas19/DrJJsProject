95a0477784f2111a955efe68dcdff9873d27bcd9
morgantrunda <morgan.trunda@marquette.edu>
2023-04-26 16:02:24 -0500

    Created the client class

---
+++ DrJJsProject/Model/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace DrJJsProject.Model
+{
+	public class Client
+	{
+		private static int autoIncrement;
+
+		public int ID { get; set; }
+
+		public string UserName { get; set; }
+		public string Password { get; set; }
+
+		public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+		public Client()
+		{
+			autoIncrement++;
+			ID = autoIncrement;
+		}
+    }
+}
+
--- DrJJsProject/Program.cs
+++ DrJJsProject/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Dr. JJ's");
+            
         }
     }
 }
