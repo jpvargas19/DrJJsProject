433b6b358928b0ba1a0e92355784f16325e1c47c
juanp <jp.vargas@marquette.edu>
2023-05-03 16:17:34 -0500

    Added the comments

--- DrJJsProject/Model/Appointment.cs
+++ DrJJsProject/Model/Appointment.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 namespace DrJJsProject.Model
 {
-    public class Appointment
+    public class Appointment // Define a new C# class called Appointment
     {
-        private static int autoIncreament;
-        public int Id { get; set; }
-        public DateTime date { get; set; }
+        private static int autoIncreament; // Declare a private static integer variable to keep track of appointment IDs
+        public int Id { get; set; } // Declare a public integer property for the appointment ID
+        public DateTime date { get; set; } // Declare a public DateTime property for the appointment date
 
-        public Appointment()
+        public Appointment() // Constructor for the Appointment class
         {
-            autoIncreament++;
-            Id = autoIncreament;
+            autoIncreament++; // Increment the static variable autoIncreament
+            Id = autoIncreament; // Set the appointment ID to the current value of autoIncreament
         }
     }
 }
