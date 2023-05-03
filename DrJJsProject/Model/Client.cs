96da2f7aa9dea98eaa271d09f8cceadcfc8e506a
dagnyschmidt <dagny.schmidt@marquette.edu>
2023-05-03 16:30:25 -0500

    hello here are my comments

--- DrJJsProject/Model/Client.cs
+++ DrJJsProject/Model/Client.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 namespace DrJJsProject.Model
 {
-    public class Client
+    public class Client //defining a new class called "Client"
     {
-        private static int autoIncreament;
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        private static int autoIncreament; //declares a static field
+        public int Id { get; set; } //declaring a public property and sets a "setter and getter"
+        public string Username { get; set; } //declaring a public property and sets a "setter and getter"
+        public string Password { get; set; } //declaring a public property and sets a "setter and getter"
+        public string FirstName { get; set; } //declaring a public property and sets a "setter and getter"
+        public string LastName { get; set; } //declaring a public property and sets a "setter and getter"
 
-        public Client()
+        public Client() //declaring a public constructor for the Client class
         {
-            autoIncreament++;
-            Id = autoIncreament;
+            autoIncreament++; //increaments the static field "autoIncreament" by 1
+            Id = autoIncreament; //assigns value to autoIncreament 
         }
     }
 }
