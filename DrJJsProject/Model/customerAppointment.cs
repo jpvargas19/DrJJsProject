262471a8c6de2e46aa0a4b14efb3ac505f351be7
dagnyschmidt <dagny.schmidt@marquette.edu>
2023-05-03 16:38:13 -0500

    hello here are my comments

--- DrJJsProject/Model/customerAppointment.cs
+++ DrJJsProject/Model/customerAppointment.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 namespace DrJJsProject.Model
 {
-        public class CustomerAppointment
+        public class CustomerAppointment //defines a new class called "CustomerAppointment"
         {
-            public Client client { get; set; }
-            public Appointment appointment { get; set; }
+            public Client client { get; set; } //declares a public property
+            public Appointment appointment { get; set; } //declares a public property
 
-            public CustomerAppointment(Client c, Appointment a)
+            public CustomerAppointment(Client c, Appointment a) //public constructor that takes two parameters
             {
-                client = c;
-                appointment = a;
+                client = c; //setting the value of client to c
+                appointment = a; //setting the value of appointment to a
             }
         }
-    
+     
 }
 
